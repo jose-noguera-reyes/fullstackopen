4d5f8c444782b90897fde9e4aaf7f4d44c212cc2
jose-noguera-reyes <noguerareyes.jose@gmail.com>
2026-05-31 18:03:14 -0300

    update part1 exercises 1.6.-1.10.

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+
+const Header = ({ text }) => {
+  console.log("header:", text)
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const StatisticLine = ({ stat, counter }) => {
+  return (
+    <>
+      <p>{stat}: {counter}</p>
+    </>
+  )
+}
+
+const Statistics = ({ statsGood, statsNeutral, statsBad }) => {
+  const all = statsGood + statsNeutral + statsBad
+  const average = (statsGood - statsBad)/all
+  const positive = (statsGood*100)/all
+  const positiveString = (String(positive) + " %")
+
+  if (all === 0) {
+    return (
+      <div>
+        No feedback given.
+      </div>
+    )
+  }
+  return (
+    <div>
+      <StatisticLine stat="Good" counter={statsGood} />
+      <StatisticLine stat="Neutral" counter={statsNeutral} />
+      <StatisticLine stat="Bad" counter={statsBad} />
+
+      <StatisticLine stat="All" counter={all}/>
+      <StatisticLine stat="Average" counter={average}/>
+      <StatisticLine stat="Positive" counter={positiveString}/>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    console.log("good before", good)
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    console.log("neutral before", neutral)
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    console.log("bad before", bad)
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header text="Unicafe - Give Feedback" />
+
+      <Button onClick={handleGoodClick} text="Good" />
+      <Button onClick={handleNeutralClick} text="Neutral" />
+      <Button onClick={handleBadClick} text="Bad" />
+
+      <Header text="Feedback Statistics" />
+
+      <Statistics statsGood={good} statsNeutral={neutral} statsBad={bad} />
+    </div>
+  )
+}
+
+export default App
